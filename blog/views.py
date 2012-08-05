cb449beacf7a86be57aef535cab26411f4e6f244
Damian <damian@lowflyingowls.co.uk>
2012-08-05 19:32:32 +0100

    Code format update. Edit post view added, not yet used or tested

--- blog/views.py
+++ blog/views.py
@@ -17,19 +17,27 @@ def new_post(request):
             form.save(request.user)
             return HttpResponseRedirect(reverse('blog.views.list_posts'))
             
-    return render_to_response('blog/new_post.html',
-            locals(), context_instance=RequestContext(request)
-    )
+    return render_to_response('blog/new_post.html', 
+                                locals(), context_instance=RequestContext(request))
 
+"""                                  
+def edit_post(request, post_url):
+    p = get_object_or_404(Post, url=post_url)
+    
+    if form.is_valid():
+        form.save(request.user)
+        return HttpResponseRedirect(reverse('blog.views.list_posts'))
+        
+    return render_to_response('blog/edit_post.html', 
+                            {'post': p}, context_instance=RequestContext(request))
+"""
 
 def list_posts(request):
     posts = Post.objects.all()
-    
-    return render_to_response('blog/list_posts.html',
-            locals(), context_instance=RequestContext(request)
-    )
+    return render_to_response('blog/list_posts.html', 
+                                locals(), context_instance=RequestContext(request))
     
 def display_post(request, post_url):
     p = get_object_or_404(Post, url=post_url)
-    return render_to_response('blog/display_post.html', {'post': p}, 
-                                  context_instance=RequestContext(request))
+    return render_to_response('blog/display_post.html', 
+                            {'post': p}, context_instance=RequestContext(request))
