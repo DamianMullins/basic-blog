b6e26ac1cc3528b715c5d4d8fba20596db2a3846
Damian <damian@lowflyingowls.co.uk>
2012-08-06 22:01:45 +0100

    Updated blog model fields

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length = 200)
-    url = models.SlugField()
+    url = models.SlugField(max_length=100, unique=True)
     content = models.TextField()
-    pub_date = models.DateTimeField('date published')
-    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User)
     def __unicode__(self):
         return self.title
