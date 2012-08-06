da0bd67d837494e80252361e7d67734169697e44
Damian <damian@lowflyingowls.co.uk>
2012-08-06 19:07:34 +0100

    Updated blog url regex

--- urls.py
+++ urls.py
@@ -4,5 +4,5 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
-    url('', include('blog.urls')),
+    url('^', include('blog.urls')),
 )
