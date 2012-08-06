08ed3b28c16178955aafdac0938e426e9c7a519e
Damian <damian@lowflyingowls.co.uk>
2012-08-06 15:01:00 +0100

    Re-ordered rules so that new post url is picked up

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import *
 from blog import views
 
 urlpatterns = patterns('blog.views', 
-    url(r'^$', 'list_posts'),
-    url(r'^(?P<post_url>[-\w]+)/$', 'display_post'),
     url(r'^new/$', 'new_post'),
+    url(r'^(?P<post_url>[-\w]+)/$', 'display_post'),
+    url(r'^$', 'list_posts'),
 )
 
 """
