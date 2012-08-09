b694a027254f272889c4c0c59f554d51d252639c
Damian <damian@lowflyingowls.co.uk>
2012-08-09 18:04:52 +0100

    Wired up delete post view url

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('blog.views',
     url(r'^new/$', 'new_post'),
     url(r'^(?P<post_url>[-\w]+)/$', 'display_post'),
     url(r'^(?P<post_url>[-\w]+)/edit/$', 'edit_post'),
+    url(r'^(?P<post_url>[-\w]+)/delete/$', 'delete_post'),
     url(r'^$', 'list_posts'),
 )
