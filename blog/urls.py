fcb0c76aa5d737441d75a43d86c96123b3743c8f
Damian <damian@lowflyingowls.co.uk>
2012-08-13 20:43:46 +0100

    Removed url function prefix

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import *
 from blog import views
 
 urlpatterns = patterns('blog.views', 
-    url(r'^new/$', 'new_post'),
-    url(r'^(?P<post_url>[-\w]+)/$', 'display_post'),
-    url(r'^(?P<post_url>[-\w]+)/edit/$', 'edit_post'),
-    url(r'^(?P<post_url>[-\w]+)/delete/$', 'delete_post'),
-    url(r'^$', 'list_posts'),
+    (r'^new/$', 'new_post'),
+    (r'^(?P<post_url>[-\w]+)/$', 'display_post'),
+    (r'^(?P<post_url>[-\w]+)/edit/$', 'edit_post'),
+    (r'^(?P<post_url>[-\w]+)/delete/$', 'delete_post'),
+    (r'^$', 'list_posts'),
 )
--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
-    url('^', include('blog.urls')),
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^', include('blog.urls')),
 )
